92ec11f5a386ce370bce67cf83e158882f16bf58
uduak <uduakumanah10@gmail.com>
2022-07-29 07:47:05 +0100

    added life cycle functions

--- app/src/main/java/com/uduak/myapplication/MainActivity.kt
+++ app/src/main/java/com/uduak/myapplication/MainActivity.kt
@@ -4,9 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    // region Set up methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+    //end Region Setup Methods
+    // end region Tear down methods
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+    // end region tear down methods
 }
