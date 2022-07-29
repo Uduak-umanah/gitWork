c596e91782575e16b63b1b2f65cffada8a9bfc24
uduak <uduakumanah10@gmail.com>
2022-07-29 10:43:18 +0100

    inseert additional files

--- app/src/main/java/com/uduak/myapplication/MainActivity.kt
+++ app/src/main/java/com/uduak/myapplication/MainActivity.kt
@@ -2,12 +2,21 @@ package com.uduak.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     // region Set up methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val zipCodeEditText = Zipcode.text
+
+        button.setOnClickListener {
+            Toast.makeText(this, "you entered ${zipCodeEditText.toString()}", Toast.LENGTH_SHORT).show()
+            }
+
+
 
     }
 
